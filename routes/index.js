bfee6d4ffda3f529faa48ced0afea82343babefd
thehanimo <hani.mohammed.3@gmail.com>
2019-12-13 07:43:56 +0530

    Add disposable email api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require("express");
 var router = express.Router();
+var https = require("https");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
+});
+
+router.get("/email", function(req, res) {
+  var email = req.query.id;
+  if (!email) {
+    res.status(500).end();
+    return;
+  }
+  var options = {
+    host: "disposable.debounce.io",
+    path: `/?email=${email}`
+  };
+  console.log(options);
+
+  https.get(options, function(res2) {
+    res2.setEncoding("utf8");
+    res2.on("data", function(chunk) {
+      res.json(JSON.parse(chunk));
+      return;
+    });
+  });
 });
 
 module.exports = router;
